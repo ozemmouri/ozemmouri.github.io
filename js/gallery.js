0ca88b544c1a03290f000744c2beb7d323b98853
Othmane Zemmouri <oth.zemmouri@gmail.com>
2023-07-12 14:40:35 +0100

    Finish the gallery set up

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,31 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  // Image info to be updated.
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+ 
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function(){
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+
+      // Change which image is current
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+
+});
+  });
 }
